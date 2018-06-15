bd54890b760805808142151b9941f74851ef2cda
Tsukasa Fujisawa <tsukasa.f@gmail.com>
2018-06-15 15:28:23 +0900

    Update Webpack config not to create source maps in Production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 /* vim:set sw=2: */
 /* eslint indent: ['error', 2] */
 
-const JS_SRC = './www/html/src/js';
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const midx = process.argv.indexOf('--mode');
+const mode = (midx > -1) ? process.argv[midx + 1] : 'development';
+const isProduction = (mode == 'production');
+
+const SRC = './www/html/src/js';
+const DIST = `${__dirname}/www/html/js`;
 
 module.exports = {
   entry: {
-    sample: `${JS_SRC}/sample.js`
+    sample: `${SRC}/sample.js`
   },
   output: {
-    path: `${__dirname}/www/html/js`,
+    path: DIST,
     filename: '[name].bundle.js'
   },
   module: {
@@ -29,5 +36,8 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-map'
+  plugins: [
+    new CleanWebpackPlugin([DIST])
+  ],
+  devtool: isProduction ? 'eval' : 'source-map'
 };
